f1c6d251f31e80d7631e6d7e64e8ceceeca6da96
Clark Egbert Alinsob <clarkalinsob101@gmail.com>
2020-01-09 16:59:53 +0800

    Update .eslintrc.js

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,6 +2,47 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
+  env: {
+    browser: true,
+    node: true,
+    mocha: true
+  },
+  globals: {
+    expect: true
+  },
   extends: ['plugin:vue/recommended', 'standard'],
-  plugins: ['vue']
+  plugins: ['vue'],
+  rules: {
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'never',
+          normal: 'any',
+          component: 'always'
+        }
+      }
+    ],
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 4,
+        multiline: {
+          max: 4,
+          allowFirstLine: true
+        }
+      }
+    ],
+    'vue/html-indent': [
+      'error',
+      2,
+      {
+        alignAttributesVertically: false
+      }
+    ],
+    'vue/html-closing-bracket-newline': 'off',
+    'no-return-assign': 'off'
+  }
 }
